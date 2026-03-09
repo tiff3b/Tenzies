a1e4919d1fd0bdf779c1a0cd9811f86400dd41b7
tiff3b <tiffany3berger@gmail.com>
2026-03-09 10:54:28 -0400

    Added dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react'
+import Die from "./Die"
 
-function App() {
-  return (
-    <main>
-<h1>hello</h1>
-    </main>
-  )
-}
+export default function App() {
+    return (
+        <main>
+            <div className="dice">
+                <Die value ={1}/>
+                <Die value ={2}/>
+                <Die value ={3}/>
+                <Die value ={4}/>
+                <Die value ={5}/>
+                <Die value ={6}/>
+                <Die value ={1}/>
+                <Die value ={1}/>
+                <Die value ={1}/>
+                <Die value ={1}/>
+            </div>
+        </main>
+)}
 
-export default App
+
